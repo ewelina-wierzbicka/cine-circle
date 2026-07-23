5581289bd5e296db025f9ce03a3b63a4d60eb573
Ewelina Wierzbicka <ewierzbicka3@gmail.com>
2026-07-23 22:25:28 +0200

    feat(skeleton): match MediaInfo structure on movie/series loading skeleton

--- src/components/MediaDetailSkeleton.tsx
+++ src/components/MediaDetailSkeleton.tsx
@@ -12,23 +12,29 @@ export function MediaDetailSkeleton() {
           <Skeleton className="aspect-2/3 w-auto md:w-full h-full md:h-auto max-w-120 rounded-2xl -rotate-[1.5deg] origin-center" />
         </div>
         <div className="flex-1 flex flex-col justify-center py-12 px-6 md:pl-6 lg:pl-12 overflow-y-auto">
-          <Skeleton className="h-4 w-44 mb-9" />
-          <div className="flex flex-wrap gap-2 mb-4">
-            <Skeleton className="h-7 w-16 rounded-full" />
-            <Skeleton className="h-7 w-20 rounded-full" />
-            <Skeleton className="h-7 w-14 rounded-full" />
+          <div className="flex flex-col w-full md:max-w-120">
+            <Skeleton className="h-4 w-44 mb-9 self-start" />
+            <div className="flex flex-wrap gap-2 mb-4">
+              <Skeleton className="h-7 w-16 rounded-full" />
+              <Skeleton className="h-7 w-20 rounded-full" />
+              <Skeleton className="h-7 w-14 rounded-full" />
+            </div>
+            <Skeleton className="h-4 w-20 mb-4" />
+            <Skeleton className="h-16 w-3/4 mb-5" />
+            <div className="flex items-center gap-5 mb-7">
+              <Skeleton className="h-4 w-28" />
+              <Skeleton className="h-1 w-1 rounded-full" />
+              <Skeleton className="h-4 w-16" />
+            </div>
+            <Skeleton className="w-12 h-px mb-8" />
+            <Skeleton className="h-4 w-full mb-2" />
+            <Skeleton className="h-4 w-full mb-2" />
+            <Skeleton className="h-4 w-5/6 mb-8" />
+            <div className="flex gap-2.5 flex-col md:flex-row">
+              <Skeleton className="h-12 rounded-xl flex-1" />
+              <Skeleton className="h-12 rounded-xl flex-1" />
+            </div>
           </div>
-          <Skeleton className="h-4 w-20 mb-4" />
-          <Skeleton className="h-16 w-3/4 mb-5" />
-          <div className="flex items-center gap-5 mb-7">
-            <Skeleton className="h-4 w-28" />
-            <Skeleton className="h-1 w-1 rounded-full" />
-            <Skeleton className="h-4 w-16" />
-          </div>
-          <Skeleton className="w-12 h-px mb-8" />
-          <Skeleton className="h-4 w-full mb-3" />
-          <Skeleton className="h-4 w-full mb-3" />
-          <Skeleton className="h-4 w-5/6" />
         </div>
       </div>
     </div>
